c3316cafc9b3130ef5f1dfd9110c2ca51d7f109d
Jackie Low <jmlow74@gmail.com>
2021-06-24 18:26:52 +0100

    for loop added to iterate over all buttons and push value to display

--- app.dev.js
+++ app.dev.js
@@ -1,28 +1,39 @@
 "use strict";
 
 //grab html elements
-var one = document.getElementById("1"); //.value;
+var one = document.getElementById("1"); //
 
-var two = document.getElementById("2").value;
-var three = document.getElementById("3").value;
-var four = document.getElementById("4").value;
-var five = document.getElementById("5").value;
-var six = document.getElementById("6").value;
-var seven = document.getElementById("7").value;
-var eight = document.getElementById("8").value;
-var nine = document.getElementById("9").value;
-var pAge = document.getElementById("p-age").value;
-var multiply = document.getElementById("multiply").value;
-var divide = document.getElementById("divide").value;
-var subtract = document.getElementById("subtract").value;
-var add = document.getElementById("add").value;
-var zero = document.getElementById("zero").value;
-var point = document.getElementById("point").value;
-var equals = document.getElementById("output"); //const plusMinus = document.getElementById("plus-minus").value;//
+var two = document.getElementById("2");
+var three = document.getElementById("3");
+var four = document.getElementById("4");
+var five = document.getElementById("5");
+var six = document.getElementById("6");
+var seven = document.getElementById("7");
+var eight = document.getElementById("8");
+var nine = document.getElementById("9");
+var pAge = document.getElementById("p-age");
+var multiply = document.getElementById("multiply");
+var divide = document.getElementById("divide");
+var subtract = document.getElementById("subtract");
+var add = document.getElementById("add");
+var zero = document.getElementById("zero");
+var point = document.getElementById("point");
+var equals = document.getElementById("output"); //const plusMinus = document.getElementById("plus-minus")//
 
 var clear = document.getElementById("clear");
-var display = document.querySelector("p");
-console.log(display);
-var clickDisplay = one.addEventListener("click", function (e) {
-  display.innerHTML = one.value;
-});
+var buttons = document.querySelectorAll("button");
+var display = document.querySelector("p"); //array to hold the values clicked
+
+var displayArr = [];
+console.log(buttons[5].value); //set up loop that targets each button, on click pushes the value of the button being iterated over to the array and displays on the "screen"
+
+var _loop = function _loop(i) {
+  buttons[i].onclick = function (e) {
+    displayArr.push(buttons[i].value);
+    display.innerHTML = displayArr;
+  };
+};
+
+for (var i = 0; i < buttons.length; i++) {
+  _loop(i);
+}
--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 //grab html elements
 
-const one = document.getElementById("1")//.value;
-const two = document.getElementById("2").value;
-const three = document.getElementById("3").value;
-const four = document.getElementById("4").value;
-const five = document.getElementById("5").value;
-const six = document.getElementById("6").value;
-const seven = document.getElementById("7").value;
-const eight = document.getElementById("8").value;
-const nine = document.getElementById("9").value;
-const pAge = document.getElementById("p-age").value;
-const multiply = document.getElementById("multiply").value;
-const divide = document.getElementById("divide").value;
-const subtract = document.getElementById("subtract").value;
-const add = document.getElementById("add").value;
-const zero = document.getElementById("zero").value;
-const point = document.getElementById("point").value;
+const one = document.getElementById("1")//
+const two = document.getElementById("2")
+const three = document.getElementById("3")
+const four = document.getElementById("4")
+const five = document.getElementById("5")
+const six = document.getElementById("6")
+const seven = document.getElementById("7")
+const eight = document.getElementById("8")
+const nine = document.getElementById("9")
+const pAge = document.getElementById("p-age")
+const multiply = document.getElementById("multiply")
+const divide = document.getElementById("divide")
+const subtract = document.getElementById("subtract")
+const add = document.getElementById("add")
+const zero = document.getElementById("zero")
+const point = document.getElementById("point")
 const equals = document.getElementById("output");
-//const plusMinus = document.getElementById("plus-minus").value;//
+//const plusMinus = document.getElementById("plus-minus")//
 const clear = document.getElementById("clear");
 
-const display = document.querySelector("p");
-console.log(display);
+const buttons = document.querySelectorAll("button");
 
-const clickDisplay = one.addEventListener("click", (e) => {
-    display.innerHTML = one.value;
-})
+const display = document.querySelector("p");
+
+//array to hold the values clicked
+const displayArr = [];
+
+console.log(buttons[5].value);
+
+//set up loop that targets each button, on click pushes the value of the button being iterated over to the array and displays on the "screen"
+for(let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = (e) => {
+    displayArr.push(buttons[i].value);
+    display.innerHTML = displayArr;
+  } 
+
+  
+}
+
